62b45172fec2e8c486ac93e75464661c5fad0659
Chris Hronek <pohek321@gmail.com>
2022-12-22 16:04:28 -0700

    Add some changes to the extract_dag

--- dags/extract_dag.py
+++ dags/extract_dag.py
@@ -3,19 +3,20 @@
 
 This DAG is used to illustrate setting an upstream dependency from the dbt DAGs. Notice the `outlets` parameter on the
 `DbtSeedOperator` objects are creating
-[Datasets](https://airflow.apache.org/docs/apache-airflow/stable/concepts/datasets.html) these are used in the `schedule`
-parameter of the dbt DAGs (`attribution-playbook`, `jaffle_shop`, `mrr-playbook`).
+[Datasets](https://airflow.apache.org/docs/apache-airflow/stable/concepts/datasets.html) these are used in the
+`schedule` parameter of the dbt DAGs (`attribution-playbook`, `jaffle_shop`, `mrr-playbook`).
 
 We're using the dbt seed command here to populate the database for the purpose of this demo. Normally an extract DAG
 would be ingesting data from various sources (i.e. sftp, blob like s3 or gcs, http endpoint, database, etc.)
 
 """
 
-from pendulum import datetime
-
 from airflow import DAG
 from airflow.datasets import Dataset
-from cosmos.providers.dbt.core.operators import DbtSeedOperator
+from airflow.utils.task_group import TaskGroup
+from pendulum import datetime
+
+from cosmos.providers.dbt.core.operators import DbtRunOperationOperator, DbtSeedOperator
 
 with DAG(
     dag_id="extract_dag",
@@ -23,18 +24,41 @@ with DAG(
     schedule="@daily",
     doc_md=__doc__,
     catchup=False,
+    max_active_runs=1,
     default_args={
         "owner": "01-EXTRACT"
-    }
+    },
 ) as dag:
 
-    for project in ['jaffle_shop', 'mrr-playbook', 'attribution-playbook']:
-        name_underscores = project.replace('-', '_')
-        DbtSeedOperator(
-            task_id=f"{name_underscores}_seed",
-            project_dir=f"/usr/local/airflow/dbt/{project}",
-            schema="public",
-            conn_id="airflow_db",
-            python_venv="/usr/local/airflow/dbt_venv/bin/activate",
-            outlets=[Dataset(f"SEED://{name_underscores.upper()}")]
-        )
+    project_seeds = [
+        {"project": "jaffle_shop", "seeds": ["raw_customers", "raw_payments", "raw_orders"]},
+        {"project": "attribution-playbook", "seeds": ["customer_conversions", "ad_spend", "sessions"]},
+        {"project": "mrr-playbook", "seeds": ["subscription_periods"]},
+    ]
+
+    with TaskGroup(group_id="drop_seeds_if_exist") as drop_seeds:
+        for project in project_seeds:
+            for seed in project["seeds"]:
+                DbtRunOperationOperator(
+                    task_id=f"drop_{seed}_if_exists",
+                    macro_name="drop_table",
+                    args={"table_name": seed},
+                    project_dir=f"/usr/local/airflow/dbt/{project['project']}",
+                    schema="public",
+                    conn_id="airflow_db",
+                    python_venv="/usr/local/airflow/dbt_venv/bin/activate",
+                )
+
+    with TaskGroup(group_id="all_seeds") as create_seeds:
+        for project in ["jaffle_shop", "mrr-playbook", "attribution-playbook"]:
+            name_underscores = project.replace("-", "_")
+            DbtSeedOperator(
+                task_id=f"{name_underscores}_seed",
+                project_dir=f"/usr/local/airflow/dbt/{project}",
+                schema="public",
+                conn_id="airflow_db",
+                python_venv="/usr/local/airflow/dbt_venv/bin/activate",
+                outlets=[Dataset(f"SEED://{name_underscores.upper()}")],
+            )
+
+    drop_seeds >> create_seeds
